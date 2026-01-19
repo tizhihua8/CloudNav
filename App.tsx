cde9a5d8b31b53d6a4a4a74ae21183ef951081db
CloudNav User <cloudnav@user.com>
2026-01-19 10:58:16 +0800

    修复构建语法错误

--- App.tsx
+++ App.tsx
@@ -1093,7 +1093,6 @@ function App() {
                                 )}
                              </div>
                         )}
-                        )}
                     </section>
                 );
             })}
